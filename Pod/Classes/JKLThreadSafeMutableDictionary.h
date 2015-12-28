b75669c15b20f13025b1544bd77b79544e013e07
jiakai lian <jiakai.lian@gmail.com>
2015-12-29 00:09:46 +1100

    Update to official header methods declaration & reformat

--- Pod/Classes/JKLThreadSafeMutableDictionary.h
+++ Pod/Classes/JKLThreadSafeMutableDictionary.h
@@ -8,31 +8,173 @@
 
 #import <Foundation/Foundation.h>
 
-@interface JKLThreadSafeMutableDictionary : NSObject //<NSCopying, NSMutableCopying, NSSecureCoding>
-//@interface JKLThreadSafeMutableDictionary: NSMutableDictionary
+NS_ASSUME_NONNULL_BEGIN
 
-+ (instancetype) dictionary;
-+ (instancetype) dictionaryWithCapacity:(NSUInteger)numItems;
-+ (instancetype) dictionaryWithDictionary:(NSDictionary *)dict;
+@interface JKLThreadSafeMutableDictionary
+    : NSObject  //<NSCopying, NSMutableCopying, NSSecureCoding,
+                //NSFastEnumeration>
 
-- (instancetype) init NS_DESIGNATED_INITIALIZER;
-- (instancetype)initWithCapacity:(NSUInteger)numItems;
-- (instancetype)initWithDictionary:(NSDictionary *)otherDictionary;
-
-#pragma mark - read operations
-- (id)objectForKey:(id)aKey;
-- (NSArray*)allValues;
-- (NSArray*)allKeys;
 - (NSUInteger)count;
+- (nullable id)objectForKey:(id<NSCopying>)aKey;
+- (NSEnumerator<id<NSCopying>> *)keyEnumerator;
+- (instancetype)init NS_DESIGNATED_INITIALIZER;
+#if TARGET_OS_WIN32
+- (instancetype)initWithObjects:(const id[])objects
+                        forKeys:(const id<NSCopying>[])keys
+                          count:(NSUInteger)cnt;
+#else
+- (instancetype)initWithObjects:(const id[])objects
+                        forKeys:(const id<NSCopying>[])keys
+                          count:(NSUInteger)cnt NS_DESIGNATED_INITIALIZER;
+#endif
+- (nullable instancetype)initWithCoder:(NSCoder *)aDecoder
+    NS_DESIGNATED_INITIALIZER;
 
-- (id)objectForKeyedSubscript:(id<NSCopying>)key;
-
-#pragma mark - write operations
-- (void)setObject:(id)obj forKey:(id<NSCopying>)key;
-- (void)removeObjectForKey:(id)key;
-- (void)removeObjectsForKeys:(NSArray*)keys;
-- (void)removeAllObjects;
-
-- (void)setObject:(id)obj forKeyedSubscript:(id<NSCopying>)key;
+- (void)removeObjectForKey:(id<NSCopying>)aKey;
+- (void)setObject:(id)anObject forKey:(id<NSCopying>)aKey;
+- (instancetype)initWithCapacity:(NSUInteger)numItems NS_DESIGNATED_INITIALIZER;
 
 @end
+
+@interface JKLThreadSafeMutableDictionary (NSExtendedDictionary)
+
+- (NSArray<id<NSCopying>> *)allKeys;
+- (NSArray<id<NSCopying>> *)allKeysForObject:(id)anObject;
+- (NSArray<id> *)allValues;
+- (NSString *)description;
+- (NSString *)descriptionInStringsFileFormat;
+- (NSString *)descriptionWithLocale:(nullable id)locale;
+- (NSString *)descriptionWithLocale:(nullable id)locale
+                             indent:(NSUInteger)level;
+- (BOOL)isEqualToDictionary:(NSDictionary<id<NSCopying>, id> *)otherDictionary;
+- (NSEnumerator<id> *)objectEnumerator;
+- (NSArray<id> *)objectsForKeys:(NSArray<id<NSCopying>> *)keys
+                 notFoundMarker:(id)marker;
+- (BOOL)writeToFile:(NSString *)path atomically:(BOOL)useAuxiliaryFile;
+- (BOOL)writeToURL:(NSURL *)url
+        atomically:(BOOL)atomically;  // the atomically flag is ignored if url
+                                      // of a type that cannot be written
+                                      // atomically.
+
+- (NSArray<id<NSCopying>> *)keysSortedByValueUsingSelector:(SEL)comparator;
+// count refers to the number of elements in the dictionary
+- (void)getObjects:(id __unsafe_unretained[])objects
+           andKeys:(id<NSCopying> __unsafe_unretained[])keys
+             count:(NSUInteger)count NS_AVAILABLE(10_7, 5_0);
+
+- (nullable id)objectForKeyedSubscript:(id<NSCopying>)key
+    NS_AVAILABLE(10_8, 6_0);
+
+- (void)enumerateKeysAndObjectsUsingBlock:
+    (void (^)(id<NSCopying> key, id obj, BOOL *stop))block
+    NS_AVAILABLE(10_6, 4_0);
+- (void)enumerateKeysAndObjectsWithOptions:(NSEnumerationOptions)opts
+                                usingBlock:(void (^)(id<NSCopying> key, id obj,
+                                                     BOOL *stop))block
+    NS_AVAILABLE(10_6, 4_0);
+
+- (NSArray<id<NSCopying>> *)keysSortedByValueUsingComparator:(NSComparator)cmptr
+    NS_AVAILABLE(10_6, 4_0);
+- (NSArray<id<NSCopying>> *)keysSortedByValueWithOptions:(NSSortOptions)opts
+                                         usingComparator:(NSComparator)cmptr
+    NS_AVAILABLE(10_6, 4_0);
+
+- (NSSet<id<NSCopying>> *)keysOfEntriesPassingTest:
+    (BOOL (^)(id<NSCopying> key, id obj, BOOL *stop))predicate
+    NS_AVAILABLE(10_6, 4_0);
+- (NSSet<id<NSCopying>> *)keysOfEntriesWithOptions:(NSEnumerationOptions)opts
+                                       passingTest:
+                                           (BOOL (^)(id<NSCopying> key, id obj,
+                                                     BOOL *stop))predicate
+    NS_AVAILABLE(10_6, 4_0);
+
+@end
+
+@interface JKLThreadSafeMutableDictionary (NSDictionaryCreation)
+
++ (instancetype)dictionary;
++ (instancetype)dictionaryWithObject:(id)object forKey:(id<NSCopying>)key;
+#if TARGET_OS_WIN32
++ (instancetype)dictionaryWithObjects:(const id[])objects
+                              forKeys:(const id<NSCopying>[])keys
+                                count:(NSUInteger)cnt;
+#else
++ (instancetype)dictionaryWithObjects:(const id[])objects
+                              forKeys:(const id<NSCopying>[])keys
+                                count:(NSUInteger)cnt;
+#endif
+
++ (instancetype)dictionaryWithObjectsAndKeys:
+    (id)firstObject, ... NS_REQUIRES_NIL_TERMINATION
+    NS_SWIFT_UNAVAILABLE("Use dictionary literals instead");
+
++ (instancetype)dictionaryWithDictionary:
+    (NSDictionary<id<NSCopying>, id> *)dict;
++ (instancetype)dictionaryWithObjects:(NSArray<id> *)objects
+                              forKeys:(NSArray<id<NSCopying>> *)keys;
+
+- (instancetype)initWithObjectsAndKeys:(id)firstObject,
+                                       ... NS_REQUIRES_NIL_TERMINATION;
+- (instancetype)initWithDictionary:
+    (NSDictionary<id<NSCopying>, id> *)otherDictionary;
+- (instancetype)initWithDictionary:
+                    (NSDictionary<id<NSCopying>, id> *)otherDictionary
+                         copyItems:(BOOL)flag;
+- (instancetype)initWithObjects:(NSArray<id> *)objects
+                        forKeys:(NSArray<id<NSCopying>> *)keys;
+
+@end
+
+@interface JKLThreadSafeMutableDictionary (NSExtendedMutableDictionary)
+
+- (void)addEntriesFromDictionary:
+    (NSDictionary<id<NSCopying>, id> *)otherDictionary;
+- (void)removeAllObjects;
+- (void)removeObjectsForKeys:(NSArray<id<NSCopying>> *)keyArray;
+- (void)setDictionary:(NSDictionary<id<NSCopying>, id> *)otherDictionary;
+- (void)setObject:(nullable id)obj
+forKeyedSubscript:(id)key NS_AVAILABLE(10_8, 6_0);
+
+@end
+
+@interface JKLThreadSafeMutableDictionary (NSMutableDictionaryCreation)
+
++ (instancetype)dictionaryWithCapacity:(NSUInteger)numItems;
+
++ (instancetype)dictionaryWithContentsOfFile:(NSString *)path;
++ (instancetype)dictionaryWithContentsOfURL:(NSURL *)url;
+- (instancetype)initWithContentsOfFile:(NSString *)path;
+- (instancetype)initWithContentsOfURL:(NSURL *)url;
+
+@end
+
+@interface JKLThreadSafeMutableDictionary (NSSharedKeySetDictionary)
+
+/*  Use this method to create a key set to pass to +dictionaryWithSharedKeySet:.
+ The keys are copied from the array and must be copyable.
+ If the array parameter is nil or not an NSArray, an exception is thrown.
+ If the array of keys is empty, an empty key set is returned.
+ The array of keys may contain duplicates, which are ignored (it is undefined
+ which object of each duplicate pair is used).
+ As for any usage of hashing, is recommended that the keys have a
+ well-distributed implementation of -hash, and the hash codes must satisfy the
+ hash/isEqual: invariant.
+ Keys with duplicate hash codes are allowed, but will cause lower performance
+ and increase memory usage.
+ */
++ (id)sharedKeySetForKeys:(NSArray<id<NSCopying>> *)keys
+    NS_AVAILABLE(10_8, 6_0);
+/*  Create a mutable dictionary which is optimized for dealing with a known set
+ of keys.
+ Keys that are not in the key set can still be set into the dictionary, but that
+ usage is not optimal.
+ As with any dictionary, the keys must be copyable.
+ If keyset is nil, an exception is thrown.
+ If keyset is not an object returned by +sharedKeySetForKeys:, an exception is
+ thrown.
+ */
++ (instancetype)dictionaryWithSharedKeySet:(id)keyset NS_AVAILABLE(10_8, 6_0);
+
+@end
+
+NS_ASSUME_NONNULL_END
--- Pod/Classes/JKLThreadSafeMutableDictionary.m
+++ Pod/Classes/JKLThreadSafeMutableDictionary.m
@@ -8,11 +8,11 @@
 
 #import "JKLThreadSafeMutableDictionary.h"
 
-static char * const QUEUE_NAME = "com.jiakai.JKLThreadSafeMutableDictionary";
+static char *const QUEUE_NAME = "com.jiakai.JKLThreadSafeMutableDictionary";
 
 @interface JKLThreadSafeMutableDictionary ()
-@property (nonatomic, strong) NSMutableDictionary * dictionary;
-@property (nonatomic, strong) dispatch_queue_t queue;
+@property(nonatomic, strong) NSMutableDictionary *dictionary;
+@property(nonatomic, strong) dispatch_queue_t queue;
 
 @end
 
@@ -20,249 +20,90 @@ static char * const QUEUE_NAME = "com.jiakai.JKLThreadSafeMutableDictionary";
 #pragma clang diagnostic ignored "-Wincomplete-implementation"
 @implementation JKLThreadSafeMutableDictionary
 
-+ (instancetype) dictionary
-{
-    return [[self alloc] init];
++ (instancetype)dictionary {
+  return [[self alloc] init];
 }
 
-+ (instancetype) dictionaryWithCapacity:(NSUInteger)numItems
-{
-    return [[self alloc] initWithCapacity:numItems];
++ (instancetype)dictionaryWithCapacity:(NSUInteger)numItems {
+  return [[self alloc] initWithCapacity:numItems];
 }
 
-+ (instancetype) dictionaryWithDictionary:(NSDictionary *)dict
-{
-    return [[self alloc] initWithDictionary:dict];
++ (instancetype)dictionaryWithDictionary:(NSDictionary *)dict {
+  return [[self alloc] initWithDictionary:dict];
 }
 
-- (instancetype)init
-{
-    self = [super init];
-    if(self)
-    {
-        _dictionary = [NSMutableDictionary dictionary];
-        _queue = dispatch_queue_create(QUEUE_NAME, DISPATCH_QUEUE_CONCURRENT);
+- (instancetype)init {
+  self = [super init];
+  if (self) {
+    _dictionary = [NSMutableDictionary dictionary];
+    _queue = dispatch_queue_create(QUEUE_NAME, DISPATCH_QUEUE_CONCURRENT);
+  }
+  return self;
+}
+
+- (instancetype)initWithCapacity:(NSUInteger)numItems {
+  self = [self init];
+  if (self) {
+    _dictionary = [NSMutableDictionary dictionaryWithCapacity:numItems];
+  }
+  return self;
+}
+
+- (instancetype)initWithDictionary:(NSDictionary *)otherDictionary {
+  self = [self init];
+  if (self) {
+    _dictionary =
+        [NSMutableDictionary dictionaryWithDictionary:otherDictionary];
+  }
+  return self;
+}
+
+- (BOOL)respondsToSelector:(SEL)aSelector {
+  if ([super respondsToSelector:aSelector]) return YES;
+
+  if ([self.dictionary respondsToSelector:aSelector]) {
+    return YES;
+  }
+
+  return NO;
+}
+
+- (NSMethodSignature *)methodSignatureForSelector:(SEL)aSelector {
+  // can this class create the signature?
+  NSMethodSignature *signature = [super methodSignatureForSelector:aSelector];
+
+  // if not, try our dictoinary
+  if (!signature) {
+    if ([self.dictionary respondsToSelector:aSelector]) {
+      return [self.dictionary methodSignatureForSelector:aSelector];
     }
-    return self;
+  }
+  return signature;
 }
 
-- (instancetype)initWithCapacity:(NSUInteger)numItems
-{
-    self = [self init];
-    if(self)
-    {
-        _dictionary = [NSMutableDictionary dictionaryWithCapacity:numItems];
+- (void)forwardInvocation:(NSInvocation *)origInvocation {
+  if ([self.dictionary respondsToSelector:[origInvocation selector]]) {
+    __weak typeof(self) weakSelf = self;
+    NSMethodSignature *methodSignature = [origInvocation methodSignature];
+    const char *type = [methodSignature methodReturnType];
+    if (*type == *@encode(void)) {
+      // write operations
+      dispatch_barrier_async(self.queue, ^{
+        __strong typeof(self) strongSelf = weakSelf;
+        [origInvocation invokeWithTarget:strongSelf.dictionary];
+      });
+    } else {
+      // read operations
+      dispatch_barrier_sync(self.queue, ^{
+        __strong typeof(self) strongSelf = weakSelf;
+        [origInvocation invokeWithTarget:strongSelf.dictionary];
+      });
     }
-    return self;
+  }
 }
 
-- (instancetype)initWithDictionary:(NSDictionary *)otherDictionary
-{
-    self = [self init];
-    if(self)
-    {
-        _dictionary = [NSMutableDictionary dictionaryWithDictionary:otherDictionary];
-    }
-    return self;
-}
-
-//
-//#pragma mark - read operations
-//
-//- (NSUInteger)count
-//{
-//    __block NSUInteger count;
-//    __weak typeof(self) weakSelf = self;
-//    dispatch_sync(self.queue, ^{
-//        __strong typeof(self) strongSelf = weakSelf;
-//        count = [strongSelf.dictioanry count];
-//    });
-//    return count;
-//}
-//
-//- (id)objectForKey:(id)key
-//{
-//    __block id obj;
-//    __weak typeof(self) weakSelf = self;
-//    dispatch_sync(self.queue, ^{
-//        __strong typeof(self) strongSelf = weakSelf;
-//        obj = [strongSelf.dictioanry objectForKey:key];
-//    });
-//    return obj;
-//}
-//
-//- (NSArray *)allKeys
-//{
-//    __block NSArray *keys;
-//    __weak typeof(self) weakSelf = self;
-//    dispatch_sync(_queue, ^{
-//        __strong typeof(self) strongSelf = weakSelf;
-//        keys = [strongSelf.dictioanry allKeys];
-//    });
-//    return keys;
-//}
-//
-//- (NSArray *)allValues
-//{
-//    __block NSArray *vals;
-//    __weak typeof(self) weakSelf = self;
-//    dispatch_sync(self.queue, ^{
-//        __strong typeof(self) strongSelf = weakSelf;
-//        vals = [strongSelf.dictioanry allValues];
-//    });
-//    return vals;
-//}
-//
-//- (id)objectForKeyedSubscript:(id<NSCopying>)key
-//{
-//    __block id obj;
-//    __weak typeof(self) weakSelf = self;
-//    dispatch_sync(self.queue, ^{
-//        __strong typeof(self) strongSelf = weakSelf;
-//        obj = [strongSelf.dictioanry objectForKeyedSubscript:key];
-//    });
-//    return obj;
-//}
-//
-//#pragma mark - write operations
-//
-//- (void)setObject:(id)obj forKey:(id)key
-//{
-//    __weak typeof(self) weakSelf = self;
-//    dispatch_barrier_async(self.queue, ^{
-//        __strong typeof(self) strongSelf = weakSelf;
-//        [strongSelf.dictioanry setObject:obj forKey:key];
-//    });
-//}
-//
-//- (void)removeObjectForKey:(id)key
-//{
-//    __weak typeof(self) weakSelf = self;
-//    dispatch_barrier_async(_queue, ^{
-//        __strong typeof(self) strongSelf = weakSelf;
-//        [strongSelf.dictioanry removeObjectForKey:key];
-//    });
-//}
-//
-//- (void)removeObjectsForKeys:(NSArray*)keys
-//{
-//    __weak typeof(self) weakSelf = self;
-//    dispatch_barrier_async(self.queue, ^{
-//        __strong typeof(self) strongSelf = weakSelf;
-//        [strongSelf.dictioanry removeObjectsForKeys:keys];
-//    });
-//}
-//
-//- (void)removeAllObjects
-//{
-//    __weak typeof(self) weakSelf = self;
-//    dispatch_barrier_async(self.queue, ^{
-//        __strong typeof(self) strongSelf = weakSelf;
-//        [strongSelf.dictioanry removeAllObjects];
-//    });
-//}
-//
-//- (NSMethodSignature *)methodSignatureForSelector:(SEL)aSelector
-//{
-//    @synchronized (delegateNodes) {
-//        for (BCGCDMulticastDelegateNode *node in delegateNodes) {
-//            NSMethodSignature *result = [node.delegate methodSignatureForSelector:aSelector];
-//            
-//            if (result != nil) {
-//                return result;
-//            }
-//        }
-//    }
-//    
-//    // This causes a crash...
-//    // return [super methodSignatureForSelector:aSelector];
-//    
-//    // This also causes a crash...
-//    // return nil;
-//    
-//    return [[self class] instanceMethodSignatureForSelector:@selector(doNothing)];
-//}
-//
-//- (void)forwardInvocation:(NSInvocation *)origInvocation {
-//  @autoreleasepool {
-//    SEL selector = [origInvocation selector];
-//    if ([self respondsToSelector:selector]) {
-//      [origInvocation invokeWithTarget:self];
-//    } else if ([self.dictioanry respondsToSelector:selector]) {
-//      __weak typeof(self) weakSelf = self;
-//      NSMethodSignature *methodSignature = [origInvocation methodSignature];
-//      dispatch_async(self.queue, ^{
-//        __strong typeof(self) strongSelf = weakSelf;
-//        [origInvocation invokeWithTarget:strongSelf.dictioanry];
-//      });
-//    } else {
-//      @throw [NSException exceptionWithName:NSInvalidArgumentException
-//                                     reason:@"Unsupported Method"
-//                                   userInfo:nil];
-//    }
-//  }
-//}
-
-- (BOOL)respondsToSelector:(SEL)aSelector
-{
-        if ([super respondsToSelector:aSelector])
-            return YES;
-    
-   if ([self.dictionary respondsToSelector:aSelector])
-        {
-            return YES;
-        }
-
-    return NO;
-}
-
-- (NSMethodSignature *)methodSignatureForSelector:(SEL)aSelector
-{
-    // can this class create the signature?
-    NSMethodSignature* signature = [super methodSignatureForSelector:aSelector];
-    
-    // if not, try our dictoinary
-    if (!signature)
-    {
-            if ([self.dictionary respondsToSelector:aSelector])
-            {
-                return [self.dictionary methodSignatureForSelector:aSelector];
-            }
-    }
-    return signature;
-}
-
-- (void)forwardInvocation:(NSInvocation *)origInvocation
-{
-        if ([self.dictionary respondsToSelector:[origInvocation selector]])
-        {
-                  __weak typeof(self) weakSelf = self;
-                  NSMethodSignature *methodSignature = [origInvocation methodSignature];
-            const char *type = [methodSignature methodReturnType];
-            if(*type== *@encode(void))
-            {
-                //write operations
-                dispatch_barrier_async(self.queue, ^{
-                    __strong typeof(self) strongSelf = weakSelf;
-                    [origInvocation invokeWithTarget:strongSelf.dictionary];
-                });
-            }
-            else
-            {
-                //read operations
-                dispatch_barrier_sync(self.queue, ^{
-                    __strong typeof(self) strongSelf = weakSelf;
-                    [origInvocation invokeWithTarget:strongSelf.dictionary];
-                });
-            }
-            
-            
-        }
-}
-
-- (void)doesNotRecognizeSelector:(SEL)aSelector
-{
-    // Prevent NSInvalidArgumentException
+- (void)doesNotRecognizeSelector:(SEL)aSelector {
+  // Prevent NSInvalidArgumentException
 }
 
 @end
