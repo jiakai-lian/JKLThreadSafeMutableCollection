a89ce0b4c301bd142429bb31f728a448827c11c9
Jacky <Jiakai.lian@gmail.com>
2015-12-30 22:48:33 +1100

    Correct the tearDown code position

--- Example/Tests/JKLThreadSafeMutableDictionaryTests.m
+++ Example/Tests/JKLThreadSafeMutableDictionaryTests.m
@@ -36,9 +36,9 @@
 
 - (void)tearDown {
     // Put teardown code here. This method is called after the invocation of each test method in the class.
-    [super tearDown];
-
     self.dic = nil;
+
+    [super tearDown];
 }
 
 #pragma mark - Initializer
