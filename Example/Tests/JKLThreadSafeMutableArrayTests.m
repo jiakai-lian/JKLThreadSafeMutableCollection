1448addd883440c099f31e741a53895cab1f2a2c
Jacky <Jiakai.lian@gmail.com>
2015-12-30 22:49:52 +1100

    Add unit tests for initialisers

--- Example/Tests/JKLThreadSafeMutableArrayTests.m
+++ Example/Tests/JKLThreadSafeMutableArrayTests.m
@@ -10,7 +10,8 @@
 #import <JKLThreadSafeMutableCollection/JKLThreadSafeMutableArray.h>
 
 @interface JKLThreadSafeMutableArrayTests : XCTestCase
-
+@property(nonatomic, strong) JKLThreadSafeMutableArray *array;
+@property(nonatomic, copy) NSArray                     *sampleArray;
 @end
 
 @implementation JKLThreadSafeMutableArrayTests
@@ -18,23 +19,70 @@
 - (void)setUp {
     [super setUp];
     // Put setup code here. This method is called before the invocation of each test method in the class.
+
+    self.sampleArray = @[@"item1", @"item2", @"item3", @"item4"];
+
+    self.array = [JKLThreadSafeMutableArray arrayWithArray:self.sampleArray];
 }
 
 - (void)tearDown {
     // Put teardown code here. This method is called after the invocation of each test method in the class.
+    self.array       = nil;
+    self.sampleArray = nil;
+
     [super tearDown];
 }
 
-- (void)testExample {
-    // This is an example of a functional test case.
-    // Use XCTAssert and related functions to verify your tests produce the correct results.
+- (void)testArray {
+    self.array = [JKLThreadSafeMutableArray array];
+
+    XCTAssertNotNil(self.array);
 }
 
-- (void)testPerformanceExample {
-    // This is an example of a performance test case.
-    [self measureBlock:^{
-        // Put the code you want to measure the time of here.
-    }];
+- (void)testArrayWithCapacity {
+    self.array = [JKLThreadSafeMutableArray arrayWithCapacity:self.sampleArray.count];
+    XCTAssertNotNil(self.array);
+}
+
+- (void)testArrayWithObject {
+    self.array = [JKLThreadSafeMutableArray arrayWithObject:self.sampleArray.firstObject];
+
+    XCTAssertNotNil(self.array);
+    XCTAssert(self.array.count == 1);
+    XCTAssert([self.array.firstObject isEqualToString:self.sampleArray.firstObject]);
+}
+
+- (void)testArrayWithArray {
+    self.array = [JKLThreadSafeMutableArray arrayWithArray:self.sampleArray];
+
+    XCTAssertNotNil(self.array);
+    XCTAssert(self.array.count == self.sampleArray.count);
+
+    for (int i = 0; i < self.array.count; ++i) {
+        XCTAssert([self.array[i] isEqualToString:self.sampleArray[i]]);
+    }
+}
+
+- (void)testInit {
+    self.array = [[JKLThreadSafeMutableArray alloc] init];
+
+    XCTAssertNotNil(self.array);
+}
+
+- (void)testInitWithCapacity {
+    self.array = [[JKLThreadSafeMutableArray alloc] initWithCapacity:self.sampleArray.count];
+    XCTAssertNotNil(self.array);
+}
+
+- (void)testInitWithArray {
+    self.array = [[JKLThreadSafeMutableArray alloc] initWithArray:self.sampleArray];
+
+    XCTAssertNotNil(self.array);
+    XCTAssert(self.array.count == self.sampleArray.count);
+
+    for (int i = 0; i < self.array.count; ++i) {
+        XCTAssert([self.array[i] isEqualToString:self.sampleArray[i]]);
+    }
 }
 
 @end
