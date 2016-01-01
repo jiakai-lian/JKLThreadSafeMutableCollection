4d5a6f9a24793e7b70d9168db3b3b9b35c36394f
Jacky <Jiakai.lian@gmail.com>
2016-01-01 13:52:58 +1100

    Complete the JKLThreadSafeMutableSet and corresponding unit tests

--- Example/Tests/JKLThreadSafeMutableSetTests.m
+++ Example/Tests/JKLThreadSafeMutableSetTests.m
@@ -10,7 +10,9 @@
 #import <JKLThreadSafeMutableCollection/JKLThreadSafeMutableSet.h>
 
 @interface JKLThreadSafeMutableSetTests : XCTestCase
-
+@property(nonatomic, strong) JKLThreadSafeMutableSet *set;
+@property(nonatomic, copy) NSSet                     *sampleSet;
+@property(nonatomic, copy) NSArray                     *sampleArray;
 @end
 
 @implementation JKLThreadSafeMutableSetTests
@@ -18,11 +20,129 @@
 - (void)setUp {
     [super setUp];
     // Put setup code here. This method is called before the invocation of each test method in the class.
+    
+    self.sampleArray = @[@"item1", @"item2", @"item3", @"item4"];
+    self.sampleSet = [NSSet setWithArray:self.sampleArray];
+    self.set = [JKLThreadSafeMutableSet setWithSet:self.sampleSet];
 }
 
 - (void)tearDown {
+    self.set = nil;
     // Put teardown code here. This method is called after the invocation of each test method in the class.
     [super tearDown];
 }
+
+- (void)testSet {
+    self.set = [JKLThreadSafeMutableSet set];
+    
+    XCTAssertNotNil(self.set);
+}
+
+- (void)testSetWithCapacity {
+    self.set = [JKLThreadSafeMutableSet setWithCapacity:self.sampleSet.count];
+    XCTAssertNotNil(self.set);
+}
+
+- (void)testSetWithObject {
+    self.set = [JKLThreadSafeMutableSet setWithObject:self.sampleArray.firstObject];
+    
+    XCTAssertNotNil(self.set);
+    XCTAssert(self.set.count == 1);
+    XCTAssert([self.set.allObjects.firstObject isEqualToString:self.sampleArray.firstObject]);
+}
+
+- (void)testSetWithArray {
+    self.set = [JKLThreadSafeMutableSet setWithArray:self.sampleArray];
+    
+    XCTAssertNotNil(self.set);
+    XCTAssert(self.set.count == self.sampleArray.count);
+    
+    for (id object in self.sampleArray) {
+        XCTAssert([self.set containsObject:object]);
+    }
+}
+
+- (void)testInit {
+    self.set = [[JKLThreadSafeMutableSet alloc] init];
+    
+    XCTAssertNotNil(self.set);
+}
+
+- (void)testInitWithCapacity {
+    self.set = [[JKLThreadSafeMutableSet alloc] initWithCapacity:self.sampleSet.count];
+    XCTAssertNotNil(self.set);
+}
+
+- (void)testInitWithArray {
+    self.set = [[JKLThreadSafeMutableSet alloc] initWithArray:self.sampleArray];
+    
+    XCTAssertNotNil(self.set);
+    XCTAssert(self.set.count == self.sampleArray.count);
+    
+    for (id object in self.sampleArray) {
+        XCTAssert([self.set containsObject:object]);
+    }
+}
+
+- (void)testDescription {
+    XCTAssertNotNil([self.set description]);
+    XCTAssert([[self.set description] isEqualToString:[self.sampleSet description]]);
+}
+
+- (void)testMultiThreading {
+    
+    static const NSUInteger DISPATCH_QUEUE_COUNT = 1000;
+    static const NSUInteger ITERATION_COUNT      = 100;
+    __weak typeof(self) weakSelf                 = self;
+    
+    for (NSUInteger i = 0; i < ITERATION_COUNT; i++) {
+        @autoreleasepool {
+            
+            self.set = [JKLThreadSafeMutableSet set];
+            
+            dispatch_queue_t queue = dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0);
+            
+            dispatch_apply(DISPATCH_QUEUE_COUNT, queue, ^(size_t i) {
+                __strong typeof(self) strongSelf = weakSelf;
+                
+                [strongSelf.set addObject:@(i)];
+                NSUInteger n = strongSelf.set.count;
+                n++;
+            });
+        }
+        
+        XCTAssertEqual(DISPATCH_QUEUE_COUNT, self.set.count);
+    }
+}
+
+- (void)testCopy {
+    NSSet *set = [self.set copy];
+    
+    XCTAssert([set isKindOfClass:[NSSet class]]);
+    
+    for (id object in self.sampleArray) {
+        XCTAssert([self.set containsObject:object]);
+    }
+}
+
+- (void)testMutableCopy {
+    NSMutableSet *set = [self.set mutableCopy];
+    
+    XCTAssert([set isKindOfClass:[NSMutableSet class]]);
+    for (id object in self.sampleArray) {
+        XCTAssert([self.set containsObject:object]);
+    }
+}
+
+- (void)testArchiveUnarchive {
+    NSData                         *data          = [NSKeyedArchiver archivedDataWithRootObject:self.set];
+    JKLThreadSafeMutableSet *unarchivedSet = [NSKeyedUnarchiver unarchiveObjectWithData:data];
+    
+    XCTAssert([unarchivedSet isKindOfClass:[JKLThreadSafeMutableSet class]]);
+    for (id object in self.sampleArray) {
+        XCTAssert([unarchivedSet containsObject:object]);
+    }
+}
+
 
 @end
--- Pod/Classes/JKLThreadSafeMutableSet.m
+++ Pod/Classes/JKLThreadSafeMutableSet.m
@@ -8,6 +8,213 @@
 
 #import "JKLThreadSafeMutableSet.h"
 
-@implementation JKLThreadSafeMutableSet
+@interface JKLThreadSafeMutableSet ()
+@property(nonatomic, strong) NSMutableSet *internalObject;
+@property(nonatomic, strong, readonly) dispatch_queue_t    queue;
 
 @end
+
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wincomplete-implementation"
+
+@implementation JKLThreadSafeMutableSet
+
+#pragma mark - Object Life Cycle
+
++ (instancetype)set {
+    return [[self alloc] init];
+}
+
++ (instancetype)setWithObject:(id)anObject {
+    return [[self alloc] initWithObject:anObject];
+}
+
++ (instancetype)setWithSet:(NSSet<id> *)set {
+    return [[self alloc] initWithSet:set];
+}
+
++ (instancetype)setWithArray:(NSArray<id> *)array {
+    return [[self alloc] initWithArray:array];
+}
+
++ (instancetype)setWithCapacity:(NSUInteger)numItems {
+    return [[self alloc] initWithCapacity:numItems];
+}
+
+- (instancetype)init {
+    self = [super init];
+    if (self) {
+        _internalObject = [NSMutableSet set];
+    }
+    return self;
+}
+
+- (instancetype)initWithObject:anObject {
+    self = [self init];
+    if (self) {
+        _internalObject = [NSMutableSet setWithObject:anObject];
+    }
+    return self;
+}
+
+- (instancetype)initWithObjects:(const id[])objects
+                          count:(NSUInteger)cnt
+{
+    self = [self init];
+    if (self) {
+        _internalObject = [[NSMutableSet alloc] initWithObjects:objects count:cnt];
+    }
+    return self;
+}
+
+- (instancetype)initWithCapacity:(NSUInteger)numItems {
+    self = [self init];
+    if (self) {
+        _internalObject = [NSMutableSet setWithCapacity:numItems];
+    }
+    return self;
+}
+
+- (instancetype)initWithArray:(NSArray<id> *)array {
+    self = [self init];
+    if (self) {
+        _internalObject = [NSMutableSet setWithArray:array];
+    }
+    return self;
+}
+
+- (instancetype)initWithSet:(NSSet<id> *)set
+                    copyItems:(BOOL)flag {
+    self = [self init];
+    if (self) {
+        _internalObject = [[NSMutableSet alloc] initWithSet:set
+                                                      copyItems:flag];
+    }
+    return self;
+}
+
+- (instancetype)initWithSet:(NSSet<id> *)set {
+    self = [self init];
+    if (self) {
+        _internalObject = [[NSMutableSet alloc] initWithSet:set];
+    }
+    return self;
+}
+
+- (id)initWithCoder:(NSCoder *)coder {
+    self = [self init];
+    if (self) {
+        _internalObject = [coder decodeObjectForKey:NSStringFromSelector(@selector(internalObject))];
+    }
+    return self;
+}
+
+#pragma mark - MultiThreading Core Methods
+
+- (dispatch_queue_t)queue {
+    static dispatch_queue_t queue;
+    static dispatch_once_t  onceToken;
+    dispatch_once(&onceToken, ^{
+        queue = dispatch_queue_create("com.jiakai.JKLThreadSafeMutableSet", DISPATCH_QUEUE_CONCURRENT);
+    });
+    
+    return queue;
+}
+
+- (BOOL)respondsToSelector:(SEL)aSelector {
+    if ([super respondsToSelector:aSelector]) return YES;
+    
+    if ([self.internalObject respondsToSelector:aSelector]) {
+        return YES;
+    }
+    
+    return NO;
+}
+
+- (NSMethodSignature *)methodSignatureForSelector:(SEL)aSelector {
+    // can this class create the signature?
+    NSMethodSignature *signature = [super methodSignatureForSelector:aSelector];
+    
+    // if not, try our internalObject
+    if (!signature) {
+        if ([self.internalObject respondsToSelector:aSelector]) {
+            return [self.internalObject methodSignatureForSelector:aSelector];
+        }
+    }
+    return signature;
+}
+
+- (void)forwardInvocation:(NSInvocation *)origInvocation {
+    if ([self.internalObject respondsToSelector:[origInvocation selector]]) {
+        __weak typeof(self) weakSelf         = self;
+        NSMethodSignature   *methodSignature = [origInvocation methodSignature];
+        const char          *type            = [methodSignature methodReturnType];
+        if (*type == *@encode(void)) {
+            // write operations
+            dispatch_barrier_async(self.queue, ^{
+                __strong typeof(self) strongSelf = weakSelf;
+                [origInvocation invokeWithTarget:strongSelf.internalObject];
+            });
+        } else {
+            // read operations
+            dispatch_sync(self.queue, ^{
+                __strong typeof(self) strongSelf = weakSelf;
+                [origInvocation invokeWithTarget:strongSelf.internalObject];
+            });
+        }
+    }
+}
+
+#pragma mark - Public Methods
+
+- (NSString *)description {
+    __block NSString *desc       = nil;
+    __weak typeof(self) weakSelf = self;
+    
+    dispatch_sync(self.queue, ^{
+        __strong typeof(self) strongSelf = weakSelf;
+        desc = [strongSelf.internalObject description];
+    });
+    
+    return desc;
+}
+
+#pragma mark - NSCoding
+
+- (void)encodeWithCoder:(NSCoder *)coder {
+    [coder encodeObject:self.internalObject
+                 forKey:NSStringFromSelector(@selector(internalObject))];
+}
+
+#pragma mark - NSCopying
+
+- (id)copyWithZone:(nullable NSZone *)zone {
+    __block id copiedItem        = nil;
+    __weak typeof(self) weakSelf = self;
+    
+    dispatch_sync(self.queue, ^{
+        __strong typeof(self) strongSelf = weakSelf;
+        copiedItem = [strongSelf.internalObject copy];
+    });
+    
+    return copiedItem;
+}
+
+#pragma mark - NSMutableCopying
+
+- (id)mutableCopyWithZone:(nullable NSZone *)zone {
+    __block id copiedItem        = nil;
+    __weak typeof(self) weakSelf = self;
+    
+    dispatch_sync(self.queue, ^{
+        __strong typeof(self) strongSelf = weakSelf;
+        copiedItem = [strongSelf.internalObject mutableCopy];
+    });
+    
+    return copiedItem;
+}
+
+
+@end
+
+#pragma clang diagnostic pop
